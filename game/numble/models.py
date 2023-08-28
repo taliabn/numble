4805b3ada6359d239ee5486e73d0d66ced5d926c
Talia <taliabennaim@gmail.com>
2023-08-27 19:46:38 -0600

    feat(Puzzle): algorithm to generate daily puzzles and solutions

--- game/numble/models.py
+++ game/numble/models.py
@@ -1,13 +1,85 @@
-# temporary constants for puzzle
+import random
+from datetime import datetime, timedelta, timezone
+from itertools import permutations
+
+
 class Puzzle:
     """
-    holds state of current puzzle
+    generates today's puzzle
     """
 
-    operations = ["+", "-", "/", "*"]
-    numbers = ["1", "2", "3", "4"]
-    target = "10"
-    answers = ["1+2+3+4", "(9+4)/(3+2)"]
+    def __init__(self) -> None:
+        self.operations = ["+", "-", "/", "*"]
+        date_string = datetime.now(tz=timezone(timedelta(hours=5))).strftime("%Y%m%d")
+        random.seed(date_string)
+        self.numbers = [str(random.randint(1, 9)) for i in range(4)]
 
+        self.generate_all_puzzles()
+        self.choose_puzzle()
 
-puzzle = Puzzle()
+    def generate_all_puzzles(self) -> None:
+        """
+        runs algorithm to generate all possible combinations of today's numbers
+        """
+        ops = [
+            lambda l, r: l + "-" + r,
+            lambda l, r: l + "+" + r,
+            lambda l, r: l + "*" + r,
+            lambda l, r: l + "/" + r,
+        ]
+        self.soln_map = {}
+        res_list = []
+
+        perms = list(permutations(self.numbers))
+        for perm in perms:
+            for op1 in ops:
+                for op2 in ops:
+                    for op3 in ops:
+                        # (a + b) + (c + d)
+                        res_list.append(
+                            op2(
+                                "(" + op1(perm[0], perm[1]) + ")", "(" + op3(perm[2], perm[3]) + ")"
+                            )
+                        )
+                        # ((a + b) + c)) + d
+                        res_list.append(
+                            op3(
+                                "(" + op2("(" + op1(perm[0], perm[1]) + ")", perm[2]) + ")", perm[3]
+                            )
+                        )
+                        # (a + (b + c)) + d
+                        res_list.append(
+                            op3(
+                                "(" + op1(perm[0], "(" + op2(perm[1], perm[2]) + ")") + ")", perm[3]
+                            )
+                        )
+                        # a + ((b + c) + d)
+                        res_list.append(
+                            op1(
+                                perm[0], "(" + op3("(" + op2(perm[1], perm[2]) + ")", perm[3] + ")")
+                            )
+                        )
+                        # a + (b + (c + d))
+                        res_list.append(
+                            op1(
+                                perm[0], "(" + op3(perm[1], "(" + op2(perm[2], perm[3]) + ")") + ")"
+                            )
+                        )
+
+        for res in res_list:
+            try:
+                res_number = eval(res)
+            except ZeroDivisionError:
+                pass
+            if not res_number % 1 and 0 < res_number < 100:  # only keep positive ints
+                if res_number in self.soln_map:
+                    self.soln_map[int(res_number)].append(res)
+                else:
+                    self.soln_map[int(res_number)] = [res]
+
+    def choose_puzzle(self) -> None:
+        """
+        picks random target, solutions pair from puzzles dictionary
+        """
+        self.target = random.choice(list(self.soln_map.keys()))
+        self.solutions = self.soln_map[self.target]
