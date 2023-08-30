a32a071a16328bd62f414b3eb148c47b9fc938a1
Talia <taliabennaim@gmail.com>
2023-08-29 20:48:26 -0600

    fix: modified settings to work for deployment

--- django-app/django-app/local_settings.py
+++ django-app/django-app/local_settings.py
@@ -3,4 +3,6 @@ from .common_settings import *
 
 DEBUG = True
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-f+2wu+u^=n65le%$lgx_=1bs#4b+j3l5)squdmeo-7@khv6%@_"
 ALLOWED_HOSTS = []
--- django-app/django-app/prod_settings.py
+++ django-app/django-app/prod_settings.py
@@ -3,8 +3,10 @@ from .common_settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["djangotest.fly.dev"]
-CSRF_TRUSTED_ORIGINS = ["https://djangotest.fly.dev"]
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = ["numble.fly.dev"]
+CSRF_TRUSTED_ORIGINS = ["https://numble.fly.dev"]
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
@@ -13,4 +15,3 @@ SECURE_HSTS_SECONDS = 60
 # SECURE_HSTS_SECONDS = 31536000 # 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
-SECURE_SSL_REDIRECT = True
